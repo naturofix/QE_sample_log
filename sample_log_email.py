9d689096313f667e7484a304e98bc392295de0a7
Shaun <shaungarnett@gmail.com>
2017-03-08 12:54:32 +0200

    os.system

--- sample_log_email.py
+++ sample_log_email.py
@@ -94,6 +94,7 @@ def copy_Ref(path,ref_path,last_time):
 	message = '\n'.join(new_raw_file_name_list)
 	cmd = "echo '%s' | mail -s 'new raw files' shaungarnett@gmail.com" %(message)
 	print cmd
+	os.system(cmd)
 
 
 
